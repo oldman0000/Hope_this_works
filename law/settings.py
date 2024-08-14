a3df0f6c844bb316414b536612cc068ce28c2dac
oldman0000 <94506732+oldman0000@users.noreply.github.com>
2024-08-14 20:28:40 +0800

    Add files via upload

--- law/settings.py
+++ law/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-oh2h*_zi5q2fr4-a+&u^(7oc42@@f_q6tx5-3=x^ir9zp0*7$h
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
--- law/wsgi.py
+++ law/wsgi.py
@@ -9,8 +9,10 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 
 import os
 
-from django.core.wsgi import get_wsgi_application
+from django.core.wsgi import get_wsgi_application # type: ignore
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'law.settings')
 
 application = get_wsgi_application()
+
+app = application
